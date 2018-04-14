588a22075d24bad76adbe23c2c47d875fd079ed2
yassine <yechabbi@gmail.com>
2018-04-14 18:55:08 +0100

    Testing fixed NPE

--- src/test/unit-tests/com/github/yassine/soxychains/subsystem/docker/networking/NetworkingImageRequirerSpec.groovy
+++ src/test/unit-tests/com/github/yassine/soxychains/subsystem/docker/networking/NetworkingImageRequirerSpec.groovy
@@ -1,13 +1,14 @@
 package com.github.yassine.soxychains.subsystem.docker.networking
 
 import com.github.yassine.soxychains.subsystem.docker.NamespaceUtils
+import com.github.yassine.soxychains.subsystem.docker.config.DockerConfiguration
 import spock.lang.Specification
 
 class NetworkingImageRequirerSpec extends Specification {
 
   def "get: it should return the soxy driver image" () {
     given:
-    def requirer = new NetworkingImageRequirer()
+    def requirer = new NetworkingImageRequirer(new DockerConfiguration())
     def image = requirer.require().blockingFirst()
     expect:
     image.getName() == NamespaceUtils.SOXY_DRIVER_NAME
